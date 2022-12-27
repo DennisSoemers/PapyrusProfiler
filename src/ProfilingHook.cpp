19c3c2f24f2aff3e4479b53b36b88dc54c04946d
nikitalita <69168929+nikitalita@users.noreply.github.com>
2022-12-26 18:19:46 -0800

    GetFunctionCallInfo() from funcCallQuery instead
    This gets us prop setters and object initialization as well

--- src/ProfilingHook.cpp
+++ src/ProfilingHook.cpp
@@ -13,6 +13,11 @@ void ProfilingHook::InstallHook() {
     auto& trampoline = SKSE::GetTrampoline();
     SKSE::AllocTrampoline(14);
 
+// Hooks into BSScript::Internal::VirtualMachine::AttemptFunctionCall
+// AttemptFunctionCall is called from from BSScript::Internal::VirtualMachine::ProcessMessageQueue
+// 1.5.97: sub_141261CB0
+// 1.6.640: sub_141388580
+// hooks at offset 0x7F (1.6.640 addr: 0x1413885FF)
     REL::Relocation<std::uintptr_t> target{RELOCATION_ID(98130, 104853), REL::VariantOffset(0x7F, 0x7F, 0x7F)};
     _original_func = trampoline.write_call<5>(target.address(), FuncCallHook);
 
@@ -28,47 +33,43 @@ void ProfilingHook::InstallHook() {
     outputLogger->set_pattern("%v");
 }
 
-static RE::BSFixedString* Profiling::FuncCallHook(std::uint64_t unk0, RE::BSScript::Stack* a_stack,
-    std::uint64_t* a_funcCallQuery) {
+static RE::BSFixedString* Profiling::FuncCallHook(
+        RE::BSScript::Internal::VirtualMachine* _this,
+        RE::BSScript::Stack* a_stack,
+        RE::BSTSmartPointer<RE::BSScript::Internal::IFuncCallQuery>& a_funcCallQuery) {
+
     if (numStacksPrinted < stacksPrintCap && a_stack) {
         // Get info from the call
-        RE::BSScript::Internal::IFuncCallQuery::CallType ignore;
+        RE::BSScript::Internal::IFuncCallQuery::CallType callType;
         RE::BSTSmartPointer<RE::BSScript::ObjectTypeInfo> scriptInfo;
-        RE::BSScript::Variable ignore2;
-        RE::BSScrapArray<RE::BSScript::Variable> ignore3;
+        RE::BSScript::Variable self;
+        RE::BSScrapArray<RE::BSScript::Variable> args;
         RE::BSFixedString functionName;
 
         const auto owningTasklet = a_stack->owningTasklet.get();
-        
-        if (owningTasklet && owningTasklet->stack) {
-            // If I don't do the following condition, the GetFunctionCallInfo() call ends up crashing
-            if (owningTasklet->stack->stackType.get() == RE::BSScript::Stack::StackType::kNormal) {
-                owningTasklet->GetFunctionCallInfo(ignore, scriptInfo, functionName, ignore2, ignore3);
+        a_funcCallQuery->GetFunctionCallInfo(callType, scriptInfo, functionName, self, args);
+        if (scriptInfo.get()) {
+            // Print this stack
+            ++numStacksPrinted;
+            std::string stackTrace = std::format("{}.{}", scriptInfo.get()->GetName(), functionName.c_str());
 
-                if (scriptInfo.get()) {
-                    // Print this stack
-                    ++numStacksPrinted;
-                    std::string stackTrace = std::format("{}.{}", scriptInfo.get()->GetName(), functionName.c_str());
-
-                    RE::BSScript::StackFrame* stackFrame = a_stack->top;
-                    if (stackFrame) {
-                        stackFrame = stackFrame->previousFrame;  // Already printed for top, so start with previous
-                        while (stackFrame) {
-                            if (stackFrame->owningFunction && stackFrame->owningFunction.get()) {
-                                const auto scriptName = stackFrame->owningFunction.get()->GetObjectTypeName().c_str();
-                                const auto funcName = stackFrame->owningFunction.get()->GetName().c_str();
-                                stackTrace = std::format("{}.{};", scriptName, funcName) + stackTrace;
-                            }
-                            stackFrame = stackFrame->previousFrame;
-                        }
+            RE::BSScript::StackFrame* stackFrame = a_stack->top;
+            if (stackFrame) {
+                stackFrame = stackFrame->previousFrame;  // Already printed for top, so start with previous
+                while (stackFrame) {
+                    if (stackFrame->owningFunction && stackFrame->owningFunction.get()) {
+                        const auto scriptName = stackFrame->owningFunction.get()->GetObjectTypeName().c_str();
+                        const auto funcName = stackFrame->owningFunction.get()->GetName().c_str();
+                        stackTrace = std::format("{}.{};", scriptName, funcName) + stackTrace;
                     }
-
-                    outputLogger->info(std::format("{} {}", stackTrace, 1));
+                    stackFrame = stackFrame->previousFrame;
                 }
             }
+
+            outputLogger->info(std::format("{} {}", stackTrace, 1));
         }
     }
 
     // Let the engine run its normal code
-    return _original_func(unk0, a_stack, a_funcCallQuery);
+    return _original_func(_this, a_stack, a_funcCallQuery);
 }
